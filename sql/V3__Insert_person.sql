f58bf2a608dccfe40dda4c92adf142a6453011fd
Derrick Wong <derrick.chwong@gmail.com>
2023-11-15 18:40:51 +0800

    add v3

---
+++ sql/V3__Insert_person.sql
@@ -0,0 +1,3 @@
+INSERT INTO person (first_name, last_name, birthday, phone_number, address) VALUES ('John', 'Doe', '1990-01-01', '1-800-555-1212', '123 Main Street');
+INSERT INTO person (first_name, last_name, birthday, phone_number, address) VALUES ('Jane', 'Smith', '1991-02-02', '1-800-555-1213', '456 Elm Street');
+INSERT INTO person (first_name, last_name, birthday, phone_number, address) VALUES ('Michael', 'Jones', '1992-03-03', '1-800-555-1214', '789 Oak Street');
